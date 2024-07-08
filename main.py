34096e54651f397129c3b08b2eb871926cb5f525
Felipe Gonzalez <gonzalezpobletefelipe@gmail.com>
2024-07-08 17:49:01 -0400

    creacion de histogramas

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # parte 4 Lectura de datos
-train_data = open('train.csv','r')
+string_train_data = open('train.txt','r')
 D = {}
 
-for i, line in enumerate(train_data):
+for i, line in enumerate(string_train_data):
     str_values = line.split(',')
     float_values = [float(x) for x in str_values]
     
@@ -14,33 +14,43 @@ for i, line in enumerate(train_data):
 
 df = pd.DataFrame(D,index=[0,1])
 
-dft=df.transpose()
+train_data = df.transpose()
 
 # fin
 
-def covariance(X:np.array,Y:np.array) -> float:
-    """ Entrega la covarianza de 2 variables aleatorias discretas
+string_val_data = open('val.txt','r')
+E = {}
 
-    Args:
-        X (np.array): arreglo de valores posibles 1
-        Y (np.array): arreglo de valores posibles 2
+for i, line in enumerate(string_val_data):
+    str_values = line.split(',')
+    float_values = [float(x) for x in str_values]
+    
+    E[i] = [float_values[0],sum(float_values,1)]
 
-    Returns:
-        float: entrega la covarianza
-    """
-    n = len(X)
-    return -(np.mean(X) * np.mean(Y)) + (np.dot(X,Y) / n) 
+df2 = pd.DataFrame(D,index=[0,1])
+
+val_data = df.transpose()
+
+# Histogramas de datos de entrenamiento
+plt.title('Histograma de porcentaje de carga de datos de entrenamiento')
+plt.hist(train_data[0],bins=100,alpha=0.5,label='Porcentaje de carga',edgecolor = "b")
+plt.legend(loc='best')
+plt.show()
+
+plt.title('Histograma de Energía consumida en datos de entrenamiento')         
+plt.hist(train_data[1],bins=500,alpha=0.5,label='Energía consumida',edgecolor = "b")     
+plt.legend(loc='best')
+plt.show()
+
+# Histogramas de datos de validacion
+plt.title('Histograma de porcentaje de carga de datos de validación')
+plt.hist(val_data[0],bins=100,alpha=0.5,label='Porcentaje de carga',edgecolor = "b")
+plt.legend(loc='best')
+plt.show()
+
+plt.title('Histograma de Energía consumida en datos de validación')         
+plt.hist(val_data[1],bins=500,alpha=0.5,label='Energía consumida',edgecolor = "b")     
+plt.legend(loc='best')
+plt.show()
 
 
-# def lerp(X: np.array,Y: np.array):
-#     """Genera una funcion que es una estimación lineal de X en función de Y,
-#     es decir, devuelve una función de la forma X=AY+b
-
-#     Args:
-#         X (np.array): _description_
-#         Y (np.array): _description_
-#     """
-print(dft.describe())
-print(covariance(dft[0],dft[1]))
-# plt.scatter(dft[0],dft[1])
-# plt.show()
