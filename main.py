7c67d26d3977c7aedb54d82fd3f7a89dc11cc5ea
Felipe Gonzalez <gonzalezpobletefelipe@gmail.com>
2024-07-07 23:21:49 -0400

    visualización de datos

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Lectura de datos
 train_data = open('train.csv','r')
 D = {}
 
-for line in train_data:
+for i, line in enumerate(train_data):
     str_values = line.split(',')
     float_values = [float(x) for x in str_values]
     
-    D[float_values[0]] = sum(float_values,1)
+    D[i] = [float_values[0],sum(float_values,1)]
 
-df = pd.DataFrame(D,index=[0])
-    
-print(df)
+df = pd.DataFrame(D,index=[0,1])
 
-print(sum([8140.13955009624,307.5538151535111,0.809257308834276,1430.7527648043197,0.0,17784.312082843033]))
+dft=df.transpose()
+print(dft.describe())
+
+
+plt.scatter(dft[0],dft[1])
+plt.show()
