bf1827f3cb77cb764dd811ebee799dcc8049317b
Felipe Gonzalez <gonzalezpobletefelipe@gmail.com>
2024-07-07 21:34:58 -0400

    lectura de datos listo& C:/Users/gonza/AppData/Local/Programs/Python/Python312/python.exe c:/Users/gonza/Desktop/Repositorios/Tarea_proba/main.py

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-training_data = pd.read_csv("train.csv")
+train_data = open('train.csv','r')
+D = {}
 
-print(training_data)
+for line in train_data:
+    str_values = line.split(',')
+    float_values = [float(x) for x in str_values]
+    
+    D[float_values[0]] = sum(float_values,1)
+
+df = pd.DataFrame(D,index=[0])
+    
+print(df)
+
+print(sum([8140.13955009624,307.5538151535111,0.809257308834276,1430.7527648043197,0.0,17784.312082843033]))
