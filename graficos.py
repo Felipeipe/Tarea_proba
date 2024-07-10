ce1a7151add81ce94b69680451cb6fe514d4410e
Felipe Gonzalez <gonzalezpobletefelipe@gmail.com>
2024-07-09 23:17:59 -0400

    tarea finalizada

--- graficos.py
+++ graficos.py
@@ -4,38 +4,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = [10, 6]
 
-print(train_data.describe())
-print(val_data.describe())
 print(f"Covarianza de X e Y del entrenamiento: {covariance(train_data[0],train_data[1])}")
 print(f"Coeficiente A del entrenamiento: {coef_A(train_data)}")
 print(f"Coeficiente b del entrenamiento: {coef_b(train_data)}")
+print(f'Error cuadrático medio de estimador lineal: {RMSE(LMMSE(train_data, val_data),val_data[0])}')
+print(f'Error cuadrático medio de estimador no lineal: {RMSE(nonlinearEst(train_data[0],train_data[1],val_data[0],val_data[1],(10000,10000)),val_data[0])}')
+print(f'Error absoluto medio de estimador lineal: {MAE(LMMSE(train_data, val_data),val_data[0])}')
+print(f'Error absoluto medio de estimador no lineal: {MAE(nonlinearEst(train_data[0],train_data[1],val_data[0],val_data[1],(10000,10000)),val_data[0])}')
 
-#Grafico de la distribucion de los datos de validacion
-#plt.title('Histograma de porcentaje de carga de datos de validación')
-#plt.hist(val_data[0],bins=1000,alpha=0.5,label='Porcentaje de carga',color='tomato',edgecolor = "tomato")
-#plt.xlabel('Porcentaje de carga')
-#plt.ylabel('Frecuencia por intervalo')
-#plt.legend(loc='best')
-#plt.show()
-#
-##Grafico de la distribucion de los datos estimados con el LMMSE
-#plt.title('Histograma de porcentaje de carga de datos de validación con LMMSE (datos de entrenamiento)')
-#plt.hist(LMMSE(train_data, val_data),bins=1000,alpha=0.5,label='Porcentaje de carga',color='tomato',edgecolor = "tomato")
-#plt.xlabel('Porcentaje de carga')
-#plt.ylabel('Frecuencia por intervalo')
-#plt.legend(loc='best')
-#plt.show()
-#
-##Grafico de la distribucion de los datos estimados con el estimador no lineal
-#plt.title('Histograma de porcentaje de carga de datos estimados con la esperanza de validación')
-#plt.hist(nonlinearEst(train_data[0],train_data[1],val_data[0],val_data[1],(10000,10000)),bins=1000,alpha=0.5,label='Porcentaje de carga',color='tomato',edgecolor = "tomato")
-#plt.xlabel('Porcentaje de carga')
-#plt.ylabel('Frecuencia por intervalo')
-#plt.legend(loc='best')
-#plt.show()
 
 # Crear una figura y ejes
-fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+fig, axs = plt.subplots(3, 1)
 
 # Gráfico de la distribución de los datos de validación
 axs[0].set_title('Histograma de porcentaje de carga de datos de validación')
@@ -60,6 +39,28 @@ axs[2].legend(loc='upper right')
 
 # Ajustar el diseño para que no se solapen los gráficos
 plt.tight_layout(pad=3.0)
+
+# Mostrar la figura completa
+plt.show()
+# Crear una figura y ejes
+fig, axs = plt.subplots(2, 1)
+
+# Gráfico de error de estimador lineal de porcentaje de carga
+axs[0].set_title('Error de LMMSE ')
+axs[0].hist((LMMSE(train_data, val_data)-val_data[0]), bins=1000, alpha=0.5, label='Porcentaje de carga', color='tomato', edgecolor="tomato")
+axs[0].set_xlabel('Porcentaje de carga')
+axs[0].set_ylabel('Frecuencia por intervalo')
+axs[0].legend(loc='upper right')
+
+# Gráfico de la distribución de los datos de validación
+axs[1].set_title('Error de estimador no lineal')
+axs[1].hist((nonlinearEst(train_data[0], train_data[1], val_data[0], val_data[1], (10000, 10000))-val_data[0]), bins=1000, alpha=0.5, label='Porcentaje de carga', color='tomato', edgecolor="tomato")
+axs[1].set_xlabel('Error de porcentaje de carga')
+axs[1].set_ylabel('Frecuencia por intervalo')
+axs[1].legend(loc='upper right')
+
+# Ajustar el diseño para que no se solapen los gráficos
+plt.tight_layout(pad=2.0)
 
 # Mostrar la figura completa
 plt.show()
